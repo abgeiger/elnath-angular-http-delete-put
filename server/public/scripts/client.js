0d01359267cf9776d43b05cebabca0e279dcbc42
Luke Schlangen <lukeschlangen@gmail.com>
2017-11-30 10:11:52 -0600

    angular get request working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,18 @@ app.controller('FoodController', ['$http', function ($http){
     console.log('Food Controller has been loaded');
     var self = this;
     self.message = 'Zip zap partner!';
+    self.foodArray = [];
+
+    self.getFood = function () {
+        $http({
+            method: 'GET',
+            url: '/food'
+        }).then(function(response) {
+            console.log('response', response.data);
+            self.foodArray = response.data;
+        });
+    };
+    
+    self.getFood();
     
 }]);
